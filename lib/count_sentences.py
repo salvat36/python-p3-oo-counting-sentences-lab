aa5036a58086cbf720133546750a5855db9e855e
Ryan <salvat36@gmail.com>
2023-05-24 11:15:36 -0500

    Passing Labs

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self.value = value
+
+  def set_value(self, value):
+    if type(value) is str:
+      self._value = value
+    else:
+      print("The value must be a string.")
+    
+  def get_value(self):
+    return self._value
+      
+  
+  def is_sentence(self):
+    return self.value.endswith(".")
+  
+  def is_question(self):
+    return self.value.endswith("?")
+  
+  def is_exclamation(self):
+    return self.value.endswith("!")
+  
+  def count_sentences(self):
+     return(len(re.findall("[.?!]+()", self.value)))
+
+  
+  value = property(get_value, set_value)
+      
+    
